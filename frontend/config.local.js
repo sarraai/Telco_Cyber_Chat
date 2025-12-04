40413b9f8362882fa0705c9a7258a3aa84eef9a1
sarraai <129076485+sarraai@users.noreply.github.com>
2025-12-04 12:53:34 +0100

    Create config.local.js

---
+++ frontend/config.local.js
@@ -0,0 +1,7 @@
+// frontend/config.local.js
+export const CONFIG = {
+  SUPABASE_URL: "https://rhobednyfefqbovouwhk.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable_zFJgwdc50wjUtqRUAjapBg_x5TwrPnX",   // your real anon key
+  LANGGRAPH_URL: "https://telco-7a28c3ff1fdd57ea956bcb342f384f8d.us.langgraph.app",
+  LANGGRAPH_API_KEY: "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",  // your real x-api-key
+};
