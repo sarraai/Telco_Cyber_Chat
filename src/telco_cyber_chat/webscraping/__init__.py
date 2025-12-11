1ffe2dfebe59657a106799a120af9f8d4e08cc50
sarraai <129076485+sarraai@users.noreply.github.com>
2025-12-11 13:33:04 +0100

    Update __init__.py

--- src/telco_cyber_chat/webscraping/__init__.py
+++ src/telco_cyber_chat/webscraping/__init__.py
@@ -1,12 +1,12 @@
 # telco_cyber_chat/webscraping/__init__.py
 
 from .scrape_core import url_already_ingested
-
 from .nokia_scraper import scrape_nokia
 from .cisco_scraper import scrape_cisco
 from .variot_scraper import scrape_variot
 from .ericsson_scraper import scrape_ericsson
 from .huawei_scraper import scrape_huawei
+from .mitre_attack_scraper import scrape_mitre_mobile
 
 __all__ = [
     "url_already_ingested",
@@ -15,4 +15,5 @@ __all__ = [
     "scrape_variot",
     "scrape_ericsson",
     "scrape_huawei",
+    "scrape_mitre_mobile",
 ]
