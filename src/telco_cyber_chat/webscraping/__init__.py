d4d92eb5814ab78475c581c717e2106fc6216cce
sarraai <129076485+sarraai@users.noreply.github.com>
2025-12-17 12:14:27 +0100

    Update __init__.py

--- src/telco_cyber_chat/webscraping/__init__.py
+++ src/telco_cyber_chat/webscraping/__init__.py
@@ -1,19 +1,6 @@
-# telco_cyber_chat/webscraping/__init__.py
+"""
+webscraping package.
 
-from .scrape_core import url_already_ingested
-from .nokia_scraper import scrape_nokia
-from .cisco_scraper import scrape_cisco
-from .variot_scraper import scrape_variot
-from .ericsson_scraper import scrape_ericsson
-from .huawei_scraper import scrape_huawei
-from .mitre_attack_scraper import scrape_mitre_mobile
-
-__all__ = [
-    "url_already_ingested",
-    "scrape_nokia",
-    "scrape_cisco",
-    "scrape_variot",
-    "scrape_ericsson",
-    "scrape_huawei",
-    "scrape_mitre_mobile",
-]
+Keep imports light to avoid slow startup / circular imports in LangGraph deployments.
+"""
+__all__ = []
